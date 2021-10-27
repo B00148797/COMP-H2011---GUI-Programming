a43dabfeae441d2506d74e700664713e287f804c
Thomas Perisse <B00148797@mytudublin.ie>
2021-10-27 22:21:33 +0100

    Lab2Part2 finished

--- Labwork 2/src/Lab2Part2.java
+++ Labwork 2/src/Lab2Part2.java
@@ -2,6 +2,23 @@ import javax.swing.*;
 
 public class Lab2Part2 extends JFrame {
     public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Lab2Part2");
 
+        jFrame.setSize(300, 300);
+
+        JPanel holderPanel = new JPanel();
+
+        JLabel labelNoImage = new JLabel("This is a label no image.");
+
+        JLabel imageLabel = new JLabel("Label with image.");
+        imageLabel.setIcon(new ImageIcon("images/Logo Google.png"));
+
+        holderPanel.add(labelNoImage);
+        holderPanel.add(imageLabel);
+
+        jFrame.add(holderPanel);
+
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
