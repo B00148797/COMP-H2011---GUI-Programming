2de6ee39f225717e1b0d84d0e8bbba143b7c2777
Thomas Perisse <B00148797@mytudublin.ie>
2021-10-27 22:25:13 +0100

    Lab2Part3 finished

--- Labwork 2/src/Lab2Part3.java
+++ Labwork 2/src/Lab2Part3.java
@@ -2,6 +2,24 @@ import javax.swing.*;
 
 public class Lab2Part3 extends JFrame {
     public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Lab2Part3");
 
+        jFrame.setSize(300, 300);
+
+        JPanel buttonPanel = new JPanel();
+
+        JButton jButtonOpen = new JButton("Open");
+        jButtonOpen.setIcon(new ImageIcon("images/Logo Open.png"));
+
+        JButton jButtonSave = new JButton("Save");
+        jButtonSave.setIcon(new ImageIcon("images/Logo Save.png"));
+
+        buttonPanel.add(jButtonOpen);
+        buttonPanel.add(jButtonSave);
+
+        jFrame.add(buttonPanel);
+
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
