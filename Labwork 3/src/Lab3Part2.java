a5041c63f76603a9ae4a49cb7ac8a53c24ea86ad
Thomas Perisse <B00148797@mytudublin.ie>
2021-10-28 13:08:22 +0100

    Lab3Part2 finished

--- Labwork 3/src/Lab3Part2.java
+++ Labwork 3/src/Lab3Part2.java
@@ -2,6 +2,35 @@ import javax.swing.*;
 
 public class Lab3Part2 extends JFrame {
     public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Lab3Part2");
 
+        jFrame.setSize(300, 300);
+
+        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
+
+        //Blanchardstown Rd N, Blanchardstown, Dublin, D15 YV78
+        JLabel jLabelAddress = new JLabel("Address:");
+        JTextField jTextFieldAddress = new JTextField("TU-Dublin Blanchardstown Rd N");
+
+        JLabel jLabelPostal = new JLabel("Postal code:");
+        JTextField jTextFieldPostal = new JTextField("D15 YV78");
+
+        JLabel jLabelCity = new JLabel("City:");
+        JTextField jTextFieldCity = new JTextField("Dublin");
+
+        JLabel jLabelCountry = new JLabel("Country:");
+        JTextField jTextFieldCountry = new JTextField("Irlande");
+
+        jFrame.add(jLabelAddress);
+        jFrame.add(jTextFieldAddress);
+        jFrame.add(jLabelPostal);
+        jFrame.add(jTextFieldPostal);
+        jFrame.add(jLabelCity);
+        jFrame.add(jTextFieldCity);
+        jFrame.add(jLabelCountry);
+        jFrame.add(jTextFieldCountry);
+
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
