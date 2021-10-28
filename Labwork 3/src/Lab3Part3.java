adb89e128976e0fa625191652e37a6d534d9c21d
Thomas Perisse <B00148797@mytudublin.ie>
2021-10-28 13:14:23 +0100

    Lab3Part3 finished

--- Labwork 3/src/Lab3Part3.java
+++ Labwork 3/src/Lab3Part3.java
@@ -1,7 +1,18 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Lab3Part3 extends JFrame {
     public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Lab3Part3");
 
+        jFrame.setSize(300, 300);
+
+        String[] chocolateBar = {"Nestle", "Kinder", "Ferrero", "Mars", "Snikers", "Milka", "KitKat", "Twix", "Bounty", "Balisto"};
+        JList<String> stringJList = new JList<>(chocolateBar);
+
+        jFrame.add(stringJList, BorderLayout.SOUTH);
+
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
