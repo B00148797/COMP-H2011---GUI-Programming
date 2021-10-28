c54ff3a6caf5bbc258f6fd629fb3ef18de431e4b
Thomas Perisse <B00148797@mytudublin.ie>
2021-10-28 13:02:07 +0100

    Lab3Part1 finished

--- Labwork 3/src/Lab3Part1.java
+++ Labwork 3/src/Lab3Part1.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import java.awt.*;
 
 public class Lab3Part1 extends JFrame {
     public static void main(String[] args) {
@@ -7,7 +6,17 @@ public class Lab3Part1 extends JFrame {
 
         jFrame.setSize(300, 300);
 
+        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
 
+        JLabel jLabel = new JLabel("Enter you Username and Password:");
+
+        JTextField jTextField = new JTextField("Username");
+
+        JPasswordField jPasswordField = new JPasswordField("Password");
+
+        jFrame.add(jLabel);
+        jFrame.add(jTextField);
+        jFrame.add(jPasswordField);
 
         jFrame.setVisible(true);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
