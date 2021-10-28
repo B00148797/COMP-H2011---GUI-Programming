2ab26d37bb805115466feb90126978a5d238352a
Thomas Perisse <B00148797@mytudublin.ie>
2021-10-28 20:39:36 +0100

    Lab5Part2 and Lab5Part3 finished

--- Labwork 5/src/Lab5Part2.java
+++ Labwork 5/src/Lab5Part2.java
@@ -1,8 +1,12 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Lab5Part2 extends JFrame implements ActionListener {
+
+    JLabel jLabel;
+    JMenuItem jMenuItemOrange, jMenuItemSFR, jMenuItemBouygues, jMenuItemFree;
 
     public Lab5Part2(){
         JFrame jFrame = new JFrame("Mobile networks");
@@ -12,32 +16,34 @@ public class Lab5Part2 extends JFrame implements ActionListener {
         JMenuBar jMenuBar = new JMenuBar();
         JMenu jMenuFrenchOperator = new JMenu("French operator");
 
-        JMenuItem jMenuItemOrange = new JMenuItem("Orange");
+        jMenuItemOrange = new JMenuItem("Orange");
         jMenuItemOrange.addActionListener(this);
         jMenuFrenchOperator.add(jMenuItemOrange);
 
-        JMenuItem jMenuItemSFR = new JMenuItem("SFR");
+        jMenuItemSFR = new JMenuItem("SFR");
         jMenuItemSFR.addActionListener(this);
         jMenuFrenchOperator.add(jMenuItemSFR);
 
-        JMenuItem jMenuItemBouygues = new JMenuItem("Bouygues Telecom");
+        jMenuItemBouygues = new JMenuItem("Bouygues Telecom");
         jMenuItemBouygues.addActionListener(this);
         jMenuFrenchOperator.add(jMenuItemBouygues);
 
-        JMenuItem jMenuItemFree = new JMenuItem("Free");
+        jMenuItemFree = new JMenuItem("Free");
         jMenuItemFree.addActionListener(this);
         jMenuFrenchOperator.add(jMenuItemFree);
 
         jMenuBar.add(jMenuFrenchOperator);
 
-        jFrame.setJMenuBar(jMenuBar);
+        jLabel = new JLabel("", SwingConstants.CENTER);
+        jFrame.add(jLabel);
 
+        jFrame.setJMenuBar(jMenuBar);
         jFrame.setVisible(true);
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
-        Lab5Part2 myLab5Part2 = new Lab5Part2();
+        Lab5Part2 lab5Part2 = new Lab5Part2();
     }
 
     @Override
@@ -45,12 +51,16 @@ public class Lab5Part2 extends JFrame implements ActionListener {
         System.out.println(actionEvent.getActionCommand());
         switch (actionEvent.getActionCommand()){
             case "Orange":
+                jLabel.setIcon(new ImageIcon(new ImageIcon("images/Orange.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
                 break;
-            case "SRF":
+            case "SFR":
+                jLabel.setIcon(new ImageIcon(new ImageIcon("images/SFR.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
                 break;
             case "Bouygues Telecom":
+                jLabel.setIcon(new ImageIcon(new ImageIcon("images/Bouygues Telecom.jpg").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
                 break;
             case "Free":
+                jLabel.setIcon(new ImageIcon(new ImageIcon("images/Free.png").getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT)));
                 break;
             default:
                 break;
--- Labwork 5/src/Lab5Part3.java
+++ Labwork 5/src/Lab5Part3.java
@@ -1,7 +1,68 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Lab5Part3 extends JFrame {
+public class Lab5Part3 extends JFrame implements ActionListener {
+
+    JCheckBoxMenuItem jCheckBoxMenuItem1;
+    JCheckBoxMenuItem jCheckBoxMenuItem2;
+    JLabel jLabel1, jLabel2;
+
+    public Lab5Part3(){
+        JFrame jFrame = new JFrame("Lab5Part3");
+        jFrame.setSize(300, 300);
+
+        JMenuBar jMenuBar = new JMenuBar();
+
+        JMenu jMenu = new JMenu("JCheckBoxMenuItem");
+
+        jCheckBoxMenuItem1 = new JCheckBoxMenuItem("JCheckBox Off");
+        jCheckBoxMenuItem1.addActionListener(this);
+        jCheckBoxMenuItem2 = new JCheckBoxMenuItem("JCheckBox Off");
+        jCheckBoxMenuItem2.addActionListener(this);
+        jMenu.add(jCheckBoxMenuItem1);
+        jMenu.add(jCheckBoxMenuItem2);
+
+        jMenuBar.add(jMenu);
+
+        jLabel1 = new JLabel("", SwingConstants.CENTER);
+        jLabel2 = new JLabel("", SwingConstants.CENTER);
+        jFrame.add(jLabel1, BorderLayout.NORTH);
+        jFrame.add(jLabel2, BorderLayout.SOUTH);
+
+        jFrame.setJMenuBar(jMenuBar);
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
     public static void main(String[] args) {
+        Lab5Part3 lab5Part3 = new Lab5Part3();
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        System.out.println(actionEvent.getActionCommand());
+        System.out.println(actionEvent.getSource());
+
+        if (actionEvent.getSource() == jCheckBoxMenuItem1){
+            if (actionEvent.getActionCommand() == "JCheckBox Off"){
+                jCheckBoxMenuItem1.setText("JCheckBox On");
+            }
+            else if(actionEvent.getActionCommand() == "JCheckBox On"){
+                jCheckBoxMenuItem1.setText("JCheckBox Off");
+            }
+            jLabel1.setText("jCheckBoxMenuItem1: " + jCheckBoxMenuItem1.getText());
+        }
+
+        if (actionEvent.getSource() == jCheckBoxMenuItem2){
+            if (actionEvent.getActionCommand() == "JCheckBox Off"){
+                jCheckBoxMenuItem2.setText("JCheckBox On");
+            }
+            else if(actionEvent.getActionCommand() == "JCheckBox On"){
+                jCheckBoxMenuItem2.setText("JCheckBox Off");
+            }
+            jLabel2.setText("jCheckBoxMenuItem2: " + jCheckBoxMenuItem2.getText());
+        }
     }
 }
