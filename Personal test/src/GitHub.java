520bcb7ecd32e1278b36310229a05bcd287ff940
Thomas Perisse <B00148797@mytudublin.ie>
2021-11-03 15:47:03 +0000

    Add top jPanel

--- Personal test/src/GitHub.java
+++ Personal test/src/GitHub.java
@@ -11,10 +11,32 @@ public class GitHub extends JFrame implements ActionListener {
         jFrameGitHub.setLocationRelativeTo(null);
         jFrameGitHub.setIconImage(new ImageIcon("images/logo.png").getImage());
 
-
+        //Add menu bar
         jFrameGitHub.setJMenuBar(GitHubMenuBar());
+
+        //Add top panel
+        jFrameGitHub.add(GitHubPanelTop());
+
         jFrameGitHub.setVisible(true);
         jFrameGitHub.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public JPanel GitHubPanelTop(){
+        JPanel jPanel = new JPanel();
+
+        String[] retositoryList = {"GUI Programming", "Interative Multimedia", "Rich Web Application", "OOAD Project"};
+        JComboBox<String> jComboBoxRepository = new JComboBox<String>(retositoryList);
+        jPanel.add(jComboBoxRepository);
+
+        String[] branchList = {"Main", "Root", "Testing", "Trash"};
+        JComboBox<String> jComboBoxBranch = new JComboBox<String>(branchList);
+        jPanel.add(jComboBoxBranch);
+
+        JButton jButton = new JButton("Fetch origin");
+        jButton.setIcon(new ImageIcon("images/load.png"));
+        jPanel.add(jButton);
+
+        return jPanel;
     }
 
     public JMenuBar GitHubMenuBar(){
@@ -31,9 +53,23 @@ public class GitHub extends JFrame implements ActionListener {
         jMenuFile.add(new JMenuItem("Exit"));
 
         JMenu jMenuEdit = new JMenu("Edit");
+        jMenuEdit.add(new JMenuItem("Undo"));
+        jMenuEdit.add(new JMenuItem("Redo"));
+        jMenuEdit.addSeparator();
+        jMenuEdit.add(new JMenuItem("Cut"));
+        jMenuEdit.add(new JMenuItem("Copy"));
+        jMenuEdit.add(new JMenuItem("Paste"));
+        jMenuEdit.add(new JMenuItem("Select all"));
+        jMenuEdit.addSeparator();
+        jMenuEdit.add(new JMenuItem("Find"));
+
+
         JMenu jMenuView = new JMenu("View");
+
         JMenu jMenuRepository = new JMenu("Repository");
+
         JMenu jMenuBranch = new JMenu("Branch");
+
         JMenu jMenuHelp = new JMenu("Help");
 
         jMenuBar.add(jMenuFile);
