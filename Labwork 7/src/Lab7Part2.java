7ef8c6652efe2680d0fffe7c5a13f22de533bfeb
Thomas Perisse <B00148797@mytudublin.ie>
2021-11-16 23:08:32 +0000

    Lab7Part2 finished

--- Labwork 7/src/Lab7Part2.java
+++ Labwork 7/src/Lab7Part2.java
@@ -1,11 +1,32 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Lab7Part2 extends JFrame implements ActionListener {
 
-    public Lab7Part2(){
+    JInternalFrame jInternalFrame;
+    Integer widthJFrame = 600, heightJFrame = 600;
+    Integer frameCount = 0;
 
+    public Lab7Part2(){
+        setTitle("Lab7Part1");
+        setSize(widthJFrame, heightJFrame);
+        setLayout(new GridLayout(5, 5));
+
+        for (int i = 0 ; i < 5 ; i++){
+            for (int j = 0 ; j < 5 ; j++){
+                frameCount++;
+                jInternalFrame = new JInternalFrame("Frame: " + frameCount);
+                //jInternalFrame.setSize(widthJFrame/5, heightJFrame/5);
+                jInternalFrame.setVisible(true);
+
+                add(jInternalFrame);
+            }
+        }
+
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
