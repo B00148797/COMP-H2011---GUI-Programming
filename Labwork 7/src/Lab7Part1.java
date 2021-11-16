94cc9d3ec05252bdbc1f22ca8abe1f81d455f735
Thomas Perisse <B00148797@mytudublin.ie>
2021-11-16 22:48:03 +0000

    Lab7Part1 finished

--- Labwork 7/src/Lab7Part1.java
+++ Labwork 7/src/Lab7Part1.java
@@ -1,11 +1,40 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Lab7Part1 extends JFrame implements ActionListener {
 
-    public Lab7Part1(){
+    JInternalFrame jInternalFrame;
+    JPanel jPanel1, jPanel2, jPanel3;
 
+    public Lab7Part1(){
+        setTitle("Lab7Part1");
+        setSize(500, 500);
+
+        jInternalFrame = new JInternalFrame("Lab7Part1 - A");
+        jInternalFrame.setSize(500, 500);
+        jInternalFrame.setVisible(true);
+
+        jPanel3 = new JPanel();
+        jPanel3.setLayout(new BoxLayout(jPanel3, BoxLayout.Y_AXIS));
+
+        jPanel1 = new JPanel();
+        jPanel1.setBackground(Color.RED);
+        jPanel1.setSize(jInternalFrame.getWidth(), jInternalFrame.getHeight()/2);
+
+        jPanel2 = new JPanel();
+        jPanel2.setBackground(Color.BLUE);
+        jPanel2.setSize(jInternalFrame.getWidth(), jInternalFrame.getHeight()/2);
+
+        jPanel3.add(jPanel1);
+        jPanel3.add(jPanel2);
+
+        jInternalFrame.add(jPanel3);
+
+        add(jInternalFrame);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
