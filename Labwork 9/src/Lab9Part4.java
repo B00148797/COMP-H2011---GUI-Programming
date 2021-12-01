64507368ade66fb976af3a978aefccbadfc12639
Thomas Perisse <B00148797@mytudublin.ie>
2021-12-01 15:41:55 +0000

    Lab9Part4 finished (cheat)

--- Labwork 9/src/Lab9Part4.java
+++ Labwork 9/src/Lab9Part4.java
@@ -1,6 +1,45 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-public class Lab9Part4 extends JFrame {
+public class Lab9Part4 extends JFrame{
+
+    public void paint(Graphics g)
+    {
+        super.paint(g);
+        g.setColor(new Color(0, 0, 0));
+        g.fillRect(125, 200, 50, 200);
+        g.fillRect(325, 200, 50, 200);
+
+        g.setColor(new Color(148, 28, 39));
+        g.fillOval(150, 150, 200, 200);
+
+        g.fillRect(200, 325, 25, 100);
+        g.fillRect(275, 325, 25, 100);
+
+        g.setColor(new Color(125, 125, 125));
+        g.fillRect(185, 325, 125, 25);
+
+        g.fillOval(245, 245-30, 10, 10);
+        g.fillOval(245, 245, 10, 10);
+        g.fillOval(245, 245+30, 10, 10);
+
+        File path = new File("images/");
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(new File(path, "Head.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        g.drawImage(image, 208, 20, null);
+
+        g.setColor(new Color(0, 125, 0));
+        g.drawString("Oh! Oh! Oh", 300, 150);
+    }
 
     public Lab9Part4(){
         setTitle("Lab9Part4");
